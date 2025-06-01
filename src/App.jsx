6cad429e6f518562d85cae7047ff2b14f702bf5e
Horia Manea <horiamanea.m@gmail.com>
2025-06-01 16:46:37 +0300

    feat: create all routes for navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BestSellers from "./pages/BestSellers";
+import New from "./pages/New";
+import Denim from "./pages/Denim";
+import Shorts from "./pages/Shorts";
+import Bottoms from "./pages/Bottoms";
+import Tops from "./pages/Tops";
+import EveryDayBasics from "./pages/EveryDayBasics";
+import Outwear from "./pages/Outwear";
+import Accessories from "./pages/Accessories";
+import Sale from "./pages/Sale";
+import ComingSoon from "./pages/ComingSoon";
 
 export default function App() {
-  return <div>App component</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/best-sellers" element={<BestSellers />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/denim" element={<Denim />} />
+        <Route path="/shorts" element={<Shorts />} />
+        <Route path="/bottoms" element={<Bottoms />} />
+        <Route path="/tops" element={<Tops />} />
+        <Route path="/every-day-basics" element={<EveryDayBasics />} />
+        <Route path="/outwear" element={<Outwear />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/sale" element={<Sale />} />
+        <Route path="/coming-soon" element={<ComingSoon />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
