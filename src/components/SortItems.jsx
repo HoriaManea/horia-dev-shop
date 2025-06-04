00ebf0afcf5f1aa24e2d74ecde7cee3ed2397c7c
Horia Manea <horiamanea.m@gmail.com>
2025-06-04 15:24:32 +0300

    feature(): implement filter & sort component

--- src/components/SortItems.jsx
+++ src/components/SortItems.jsx
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpWideShort } from "@fortawesome/free-solid-svg-icons";
+
+const SortTtemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Icon = styled(FontAwesomeIcon)`
+  font-size: 25px;
+`;
+
 function SortItems() {
-  return <div>sort items</div>;
+  return (
+    <SortTtemContainer>
+      <Icon icon={faArrowUpWideShort} />
+      <p>FILTER & SORT</p>
+    </SortTtemContainer>
+  );
 }
 
 export default SortItems;
